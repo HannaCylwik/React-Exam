78927078741df559963917afd09ab985d0846756
Hanna Cylwik <hanna.cylwik@wp.pl>
2020-07-19 21:58:54 +0200

    add alert

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -2,8 +2,18 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import style from "./Menu.module.css";
 import cart from "../../bag.svg";
+import { useSelector } from "react-redux";
 
 const Menu = () => {
+  const shopCart = useSelector((state) => state.productsInCart);
+
+  const shopCartClick = () => {
+    let price = 0;
+    shopCart.forEach(shop => {
+      price += Number(shop.amount)
+    });
+    alert(`Ilość przedmiotów w koszyku: ${shopCart.length}, Wartość przedmiotów w koszyku: ${price}`)
+  }
   return (
     <div>
       <nav className={style.navigation}>
@@ -24,7 +34,7 @@ const Menu = () => {
             </NavLink>
           </li>
         </ul>
-        <img src={cart} height="50px" className={style.navigation_img} />
+        <img src={cart} height="50px" className={style.navigation_img} onClick={shopCartClick} />
       </nav>
     </div>
   );
